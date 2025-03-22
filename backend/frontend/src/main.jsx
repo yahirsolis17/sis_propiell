6f759db6070e5ec8411f790edebd5ab56104d919
Yahir <178798132+yahirsolis17@users.noreply.github.com>
2025-03-22 02:47:43 -0600

    delete

--- backend/frontend/src/main.jsx
+++ backend/frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './bootstrap-icons-fix.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import "animate.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(

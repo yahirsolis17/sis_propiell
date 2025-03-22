679c2b250ccd6cf732eac3d4698d7b33e2cf79d1
Yahir <178798132+yahirsolis17@users.noreply.github.com>
2025-03-22 04:12:54 -0600

    update

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -6,17 +6,28 @@ export default defineConfig({
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    cssMinify: false, // Desactiva minificación CSS temporalmente
     rollupOptions: {
       output: {
-        // Mantén nombres originales para fuentes
         assetFileNames: (assetInfo) => {
+          // Mantén nombres originales para fuentes
           if (assetInfo.name.endsWith(".woff2") || assetInfo.name.endsWith(".woff")) {
             return "assets/[name][extname]";
           }
+          // Hashea otros assets
           return "assets/[name]-[hash][extname]";
         }
       }
     }
   },
-  assetsInclude: ["**/*.woff", "**/*.woff2"]
+  assetsInclude: ["**/*.woff", "**/*.woff2"],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://127.0.0.1:8000",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });

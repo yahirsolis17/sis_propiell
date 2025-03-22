0c25e13b1de04a4237519b2e44933795e6697d61
yahirsolis17 <22307013@utcgg.edu.mx>
2025-03-22 02:02:27 -0600

    Update App.jsx

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Register from "./pages/Register"; // 🔹 Importa la página de registro
+import Register from "./pages/Register";
 import Dashboard from "./pages/Dashboard";
 import Citas from './pages/Citas';
 import PaymentPage from './pages/PaymentPage';
 
 function App() {
+  // Ejemplo: Leer la variable de entorno
+  const baseUrl = import.meta.env.VITE_API_URL;
+
+  console.log("Base URL del backend:", baseUrl);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} /> {/* 🔹 Nueva ruta */}
+        <Route path="/register" element={<Register />} />
         <Route path="/dashboard/:role" element={<Dashboard />} />
         <Route path="/citas" element={<Citas />} />
         <Route path="/pago/:citaId" element={<PaymentPage />} />

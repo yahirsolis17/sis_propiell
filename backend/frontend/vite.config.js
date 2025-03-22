0e45800a78ef05baaa3bccdeee8780bf65ff868a
yahirsolis17 <22307013@utcgg.edu.mx>
2025-03-21 23:45:53 -0600

    Update vite.config.js

--- backend/frontend/vite.config.js
+++ backend/frontend/vite.config.js
@@ -3,10 +3,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: "dist", // 👈 asegura que se compile ahí
+  },
+  base: "./", // 👈 necesario si usas rutas relativas en React
   server: {
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:8000",
+        target: "http://127.0.0.1:8000", // solo para desarrollo local
         changeOrigin: true,
         secure: false,
       },

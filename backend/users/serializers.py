75417190d5d0b50742af361dfe7ad08f009a5bc1
Yahir Solis <yahirsolis546@gmail.com>
2025-12-03 06:33:18 -0600

    solucion

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -36,6 +36,46 @@ class BaseUserSerializer(serializers.ModelSerializer):
         ]
 
 
+class UserSerializer(serializers.ModelSerializer):
+    """CRUD completo para administración de usuarios."""
+
+    password = serializers.CharField(write_only=True, required=False, allow_blank=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "password",
+            "nombre",
+            "apellidos",
+            "edad",
+            "sexo",
+            "peso",
+            "telefono",
+            "role",
+            "especialidad",
+            "is_active",
+            "is_staff",
+        ]
+
+    def create(self, validated_data):
+        password = validated_data.pop("password", None)
+        user = User.objects.create_user(**validated_data)
+        if password:
+            user.set_password(password)
+            user.save(update_fields=["password"])
+        return user
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if password:
+            instance.set_password(password)
+        instance.save()
+        return instance
+
+
 class DoctorSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
